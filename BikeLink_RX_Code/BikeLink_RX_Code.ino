e5728a9fb8ff93d4e3c6d926f83ca47828a0ae66
Tommi <T-o-m-m-i@users.noreply.github.com>
2017-05-21 14:09:31 +0300

    RHLib

--- BikeLink_RX_Code/BikeLink_RX_Code.ino
+++ BikeLink_RX_Code/BikeLink_RX_Code.ino
@@ -1,114 +1,245 @@
+#include "Libs/RHReliableDatagram.h"
+#include "Libs/RH_RF95.h"
+#include <ShiftDisplay.h>
 #include <SPI.h>
-#include <LoRa.h>
+#include "Libs/ShiftRegister74HC595.h"
 
-char Received_TXT;
-bool Receiver_responses_OK = false;
-bool connection_OK = false;
-bool ALARM = false;
-//char akun_varaus = "A9";
-uint16_t jannite_raja = 700;
+#define CLIENT_ADDRESS 1
+#define SERVER_ADDRESS 2
 
-void setup() {
-  Serial.begin(9600);
-  while (!Serial);
+#define RFM95_RST 9
+#define RF95_FREQ 868.0
+#define RF95_PWR 17
+#define BATTERY_PIN A0
+#define BTN_INTERRUPT_PIN 3
+#define BUZZER_PIN A3
+#define LATCH_PIN 7
+#define SCL_PIN 6
+#define SDA_PIN 5
 
-  Serial.println("LoRa Receiver");
+uint8_t * digitalValues;
+//char const handShakeCode[] = "6d950a629f27ee411602f1640a8492b1";
 
-  if (!LoRa.begin(866E6)) {
-    Serial.println("Starting LoRa failed!");
-    while (1);
-  }
-  //First, after powering on, try to make handling with transmitter. Send message untill transmitter responses
-    while(!connection_OK)
-    {
-      //Serial.println("Connecting...");
-          // send packet
-          //LoRa.beginPacket();
-          //LoRa.print("Establish connection?");
-          //LoRa.endPacket();
-          //delay(1000);
-       // Received response from transmitter??
-          int packetSize = LoRa.parsePacket();
-          if (packetSize) {
-            char paketti [packetSize];
-            char paketti2 [packetSize];
-            memset(paketti2, 0, sizeof(paketti2));
-            int i=0;
-              // received a packet
-              Serial.print("Received packet '");
+// create shift register object (number of shift registers, data pin, clock pin, latch pin)
+//ShiftRegister74HC595 led (3, 5, 6, 7);
 
-              // read packet
-                  while (LoRa.available()) {
-                  paketti[i] = (char)LoRa.read(); //TRANSMITTER SHOULD SEND OK OR SOMETHING?
-                  i++;
+ShiftDisplay led(LATCH_PIN, SCL_PIN, SDA_PIN, COMMON_ANODE, 3);
 
-                  //if (LoRa.read() == "YES,establish")
-                  //  Receiver_responses_OK = 1;
-                  }
-                  memcpy(paketti2, paketti, 5);
-                  Serial.println(paketti2);
+struct dataStruct
+{
+	bool alarmState;
+	uint16_t TX_Voltage; //ADC = 10-bit = 0-1023
 
-                  if ((String)paketti2 == "TESTI")
-                  {
-                    Serial.print ("HELLO KUNINGAS");
-                    Receiver_responses_OK = 1;
-                  }
-          }
+} SensorReadings;
 
-          if (Receiver_responses_OK == 1)
-          {
-            connection_OK = 1;
-            // Connection is now established.
-             delay(1000);
-            //Set receiver mode
-             LoRa.receive();
-             ALARM = 0;
-             //Arduino to power saving mode. Can be waken up with WDT, how about RFM95???
-             //sleep();
+uint16_t RX_Voltage;
+volatile uint8_t buttonState = 0;
+volatile boolean alarmState = false;
+bool TX_responded_OK = false;
 
-            break;
-          }
-    }
+// Instance of the radio driver
+RH_RF95 driver;
+
+// Class to manage message delivery and receipt, using the driver declared above
+RHReliableDatagram manager(driver, SERVER_ADDRESS);
+
+// Run this code if Button Interrupt was triggered
+void buttonInterrupt(void)
+{
+	detachInterrupt(digitalPinToInterrupt(BTN_INTERRUPT_PIN));
+	buttonState++;
+	if( buttonState % 4 == 0 )
+		buttonState = 0;
+
+	Serial.print("Button: ");
+	Serial.print(buttonState);
+	Serial.println("");
+	delay(100);
+	attachInterrupt(digitalPinToInterrupt(BTN_INTERRUPT_PIN), buttonInterrupt, RISING);
 }
 
+void setup()
+{
+	//ADC0
+	pinMode(A0, INPUT);
 
-void loop() {
-  // try to parse packet
-  int packetSize = LoRa.parsePacket();
-  if (packetSize) {
-    char paketti [packetSize];
-    char paketti2 [packetSize];
-    memset(paketti2, 0, sizeof(paketti2));
-    int i=0;
+	//Button pin
+	pinMode(3,INPUT_PULLUP);
 
-    // received a packet
-    Serial.print("Received packet '");
+	//Buzzer pin
+	pinMode(BUZZER_PIN, OUTPUT);
 
-    // read packet
-    while (LoRa.available()) {
-      Received_TXT = (char)LoRa.read();
-      paketti[i] = Received_TXT;
-      i++;
-      Serial.print(Received_TXT);
-    }
+	//Display pins
+	/*pinMode(LATCH_PIN, OUTPUT);
+	pinMode(SCL_PIN, OUTPUT);
+	pinMode(SDA_PIN, OUTPUT);*/
 
-    // print RSSI of packet
-    Serial.print("' with RSSI ");
-    Serial.print(LoRa.packetRssi());
+	//Reset pin
+	pinMode(RFM95_RST, OUTPUT);
+	digitalWrite(RFM95_RST, HIGH);
 
-    Serial.println();
-    Serial.println("paketti");
-    Serial.println(paketti);
-    memcpy(paketti2, paketti, 5);
+	//Manual reset the radio
+	digitalWrite(RFM95_RST, LOW);
+	delay(10);
+	digitalWrite(RFM95_RST, HIGH);
+	delay(10);
 
-    Serial.println("paketti2");
-    Serial.println(paketti2);
-    if ((String)paketti2 == "hello")
-      {
-       Serial.print ("HELLO KUNINGAS");
-      }
+	Serial.begin(9600);
+	while (!Serial) ; // Wait for serial port to be available
 
-  Serial.println();
-  Serial.println();
-  }
+	// Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+	if (!manager.init())
+		Serial.println("init failed");
+
+	if (!driver.setFrequency(RF95_FREQ)) {
+		while (1);
+	}
+
+	// The default transmitter power is 13dBm, using PA_BOOST.
+	// If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
+	// you can set transmitter powers from 5 to 23 dBm:
+	driver.setTxPower(17, false);
+	// If you are using Modtronix inAir4 or inAir9,or any other module which uses the
+	// transmitter RFO pins and not the PA_BOOST pins
+	// then you can configure the power transmitter power for -1 to 14 dBm and with useRFO true.
+	// Failure to do that will result in extremely low transmit powers.
+	//  driver.setTxPower(14, true);
+	// You can optionally require this module to wait until Channel Activity
+	// Detection shows no activity on the channel before transmitting by setting
+	// the CAD timeout to non-zero:
+	//driver.setCADTimeout(1000);
+	manager.setRetries(5);
+	manager.setTimeout(500);
+
+	attachInterrupt(digitalPinToInterrupt(BTN_INTERRUPT_PIN), buttonInterrupt, RISING);
+
+
+	// allocates the specified number of bytes and initializes them to zero
+	digitalValues = (uint8_t *)malloc(3 * sizeof(uint8_t));
+	memset(digitalValues, 0, 3 * sizeof(uint8_t));
+
+
+}
+
+uint8_t data[] = "And hello back!";
+// Dont put this on the stack:
+uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+
+void loop()
+{
+	// Don't put this on the stack:
+	uint8_t buf[sizeof(SensorReadings)];
+	uint8_t len = sizeof(buf);
+	uint8_t from = 0x00;
+
+	if (manager.available())
+	{
+		Serial.println("RX manager.available()");
+		// Wait for a message addressed to us from the client
+		if (manager.recvfromAck(buf, &len, &from))
+		{
+			memcpy(&SensorReadings, buf, sizeof(SensorReadings));
+
+			Serial.print("RX got message from : 0x");
+			Serial.print(from, HEX);
+			Serial.print(": ");
+			Serial.print(SensorReadings.TX_Voltage);
+			Serial.print(": ");
+			Serial.println(SensorReadings.alarmState);
+
+			if(!alarmState)
+				alarmState = SensorReadings.alarmState;
+
+			//while(!manager.sendtoWait(data, sizeof(data), from))
+			//{
+			//	Serial.println("RX Failed 'sendtoWait'");
+			//}
+			//Serial.println("RX Succesfull 'sendtoWait'");
+
+
+			// Send a reply back to the originator client
+			//if (!manager.sendtoWait(data, sizeof(data), from))
+			//  Serial.println("RX sendtoWait failed");
+		}
+	}
+
+	if(alarmState)
+	{
+		//Buzzzz
+		alarmSound();
+	}
+
+	updateDisplay(buttonState);
+
+}
+
+void updateDisplay(uint8_t state)
+{
+	switch(state)
+	{
+		case 0:{
+				led.print(1000, 1);
+				led.print(1000, 2);
+				led.print(1000, 3);
+				led.print(1000, 4);
+				led.print(1000, 5);
+				led.print(1000, "001", ALIGN_RIGHT);
+
+
+				//Display off
+				//display(0);
+				//led.print(1000, "941");
+				break;}
+		case 1:{
+				//Display off
+				//display(1);
+				//long raw = SensorReadings.TX_Voltage;
+				//long voltage = map(raw, 0, 1020, 0, 420);
+				led.set("G");
+				led.show(1000);
+				break;}
+		case 2:{
+				//Display off
+				//display(1);
+				readVoltage();
+				//long raw = RX_Voltage;
+				//long voltage = map(raw, 0, 1020, 0, 420);
+				digitalWrite(LATCH_PIN, LOW);
+				int byte;
+				digitalValues[0]=B11111100;
+				digitalValues[1]=B01100000;
+				digitalValues[2]=B11011010;
+
+				for (byte = 3 - 1; byte >= 0; byte--)
+				{
+					shiftOut(SDA_PIN, SCL_PIN, LSBFIRST, ~digitalValues[byte]);
+				}
+				digitalWrite(LATCH_PIN, HIGH);
+				digitalWrite(LATCH_PIN, LOW);
+				break;}
+		default:
+				//Display off
+				//display(0);
+
+				break;
+
+	}
+	//attachInterrupt(digitalPinToInterrupt(BTN_INTERRUPT_PIN), buttonInterrupt, RISING);
+}
+
+void alarmSound(void)
+{
+	// play a note on pin 7 for 500 ms:
+	tone(BUZZER_PIN, 494, 500);
+	delay(500);
+	tone(BUZZER_PIN, 294, 500);
+	delay(500);
+
+	// turn off tone function for pin 7:
+	noTone(BUZZER_PIN);
+}
+
+void readVoltage()
+{
+	RX_Voltage = analogRead(BATTERY_PIN);
 }
--- BikeLink_TX_Code/BikeLink_TX_Code.ino
+++ BikeLink_TX_Code/BikeLink_TX_Code.ino
@@ -1,42 +1,307 @@
-#include "Arduino.h"
+#include "Libs/RHReliableDatagram.h"
+#include "Libs/RH_RF95.h"
+#include "Libs/Bildr_ADXL345.h"
 #include <SPI.h>
-#include <LoRa.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
+#include <avr/power.h>
+#include <avr/wdt.h>
 
-int counter = 0;
+#define CLIENT_ADDRESS 1
+#define SERVER_ADDRESS 2
 
-void setup() {
-  Serial.begin(9600);
-  while (!Serial);
+#define RFM95_RST 9
+#define RF95_FREQ 868.0
+#define RF95_PWR 17
+#define BATTERY_PIN A0
+#define ACC_INTERRUPT_PIN 3
 
-  Serial.println("LoRa Sender");
+volatile boolean activity = false;
+volatile boolean activited = false;
+volatile bool watchdogActivated = false;
+uint8_t sleepIterations = 0;
+volatile uint8_t maxSleepIterations = 3;
 
-  if (!LoRa.begin(866E6)) {
-    Serial.println("Starting LoRa failed!");
-    while (1);
-  }
+//char const handShakeCode[] = "6d950a629f27ee411602f1640a8492b1";
+
+struct dataStruct
+{
+	bool alarmState;
+	uint16_t TX_Voltage; //ADC = 10-bit = 0-1023
+
+} SensorReadings;
+
+bool RX_responded_OK = false;
+
+uint8_t data[] = "Hello World!";
+// Dont put this on the stack:
+//uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+uint8_t buf[sizeof(SensorReadings)] = {0};
+
+
+// Singleton instance of the radio driver
+RH_RF95 driver;
+//RH_RF95 driver(5, 2); // Rocket Scream Mini Ultra Pro with the RFM95W
+
+// Class to manage message delivery and receipt, using the driver declared above
+RHReliableDatagram manager(driver, CLIENT_ADDRESS);
+
+ADXL345 adxl;
+
+// Run this code if Activity Interrupt was triggered
+void activityInterrupt(void)
+{
+	detachInterrupt(digitalPinToInterrupt(ACC_INTERRUPT_PIN));
+
+	activity = true;
+	SensorReadings.alarmState = activity;
+
+	//Set WDT faster
+	maxSleepIterations = 1;
 }
 
-void loop() {
-  Serial.print("Sending packet: ");
-  Serial.println(counter);
+// The Watchdog Time-out Interrupt
+ISR(WDT_vect)
+{
+	watchdogActivated = true;
+}
 
-  // send packet
+void setup()
+{
+	SensorReadings.TX_Voltage = 0;
+	SensorReadings.alarmState = false;
 
-  if (counter == 4){
-  LoRa.beginPacket();
-  LoRa.print("TESTIVIESTI");
-  LoRa.endPacket();
-  }
-  else{
-  LoRa.beginPacket();
-  LoRa.print("hello ");
-  LoRa.print(counter);
-  LoRa.endPacket();
-  }
+	//Interrupt pin
+	pinMode(ACC_INTERRUPT_PIN, INPUT);
+	//ADC0
+	pinMode(BATTERY_PIN, INPUT);
+	//Reset pin
+	pinMode(RFM95_RST, OUTPUT);
+	digitalWrite(RFM95_RST, HIGH);
+
+	// manual reset
+	digitalWrite(RFM95_RST, LOW);
+	delay(10);
+	digitalWrite(RFM95_RST, HIGH);
+
+	delay(10);
+
+	// Rocket Scream Mini Ultra Pro with the RFM95W only:
+	// Ensure serial flash is not interfering with radio communication on SPI bus
+	//  pinMode(4, OUTPUT);
+	//  digitalWrite(4, HIGH);
+
+	Serial.begin(9600);
+	while (!Serial) ; // Wait for serial port to be available
+
+	// Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+	if (!manager.init())
+		Serial.println("init failed");
+
+	if (!driver.setFrequency(RF95_FREQ)) {
+		while (1);
+	}
+
+	// The default transmitter power is 13dBm, using PA_BOOST.
+	// If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
+	// you can set transmitter powers from 5 to 23 dBm:
+	driver.setTxPower(17, false);
+	// If you are using Modtronix inAir4 or inAir9,or any other module which uses the
+	// transmitter RFO pins and not the PA_BOOST pins
+	// then you can configure the power transmitter power for -1 to 14 dBm and with useRFO true.
+	// Failure to do that will result in extremely low transmit powers.
+	//  driver.setTxPower(14, true);
+	// You can optionally require this module to wait until Channel Activity
+	// Detection shows no activity on the channel before transmitting by setting
+	// the CAD timeout to non-zero:
+	//driver.setCADTimeout(1000);
+
+	manager.setRetries(5);
+	manager.setTimeout(500);
+
+	accSetup();
+
+	// Setup the watchdog timer to run an interrupt which
+	// wakes the Arduino from sleep every 8 seconds.
+
+	// Note that the default behavior of resetting the Arduino
+	// with the watchdog will be disabled.
+
+	// This next section of code is timing critical, so interrupts are disabled.
+	// See more details of how to change the watchdog in the ATmega328P datasheet
+	// around page 50, Watchdog Timer.
+	noInterrupts();
+
+	// Set the watchdog reset bit in the MCU Status Segister to 0.
+	MCUSR &= ~(1<<WDRF);
+
+	// Set WDCE and WDE bits in the Watchdog Timer Control Register.
+	WDTCSR |= (1<<WDCE) | (1<<WDE);
+
+	// Set Watchdog Clock Prescaler bits to a value of 8 seconds.
+	WDTCSR = (1<<WDP0) | (1<<WDP3);
+
+	// Enable watchdog as interrupt only (no reset).
+	WDTCSR |= (1<<WDIE);
+
+	// Enable interrupts again.
+	interrupts();
+
+	attachInterrupt(digitalPinToInterrupt(ACC_INTERRUPT_PIN), activityInterrupt, RISING);
+	handShake();
+}
+
+void loop()
+{
+	if(watchdogActivated || (activity && !activited))
+	{
+		watchdogActivated = false;
+		sleepIterations += 1;
+
+		if((sleepIterations >= maxSleepIterations) || (activity && !activited))
+		{
+			if(activity)
+				activited = true;
+
+			// Reset the number of sleep iterations.
+			sleepIterations = 0;
+
+			Serial.println("TX In loop() ");
+
+			readVoltage();
 
 
-  counter++;
+			//int x,y,z;
+			//adxl.readAccel(&x, &y, &z);
 
-  delay(5000);
+			// Output x,y,z values - Commented out
+			//Serial.print("ALARM: "); Serial.print(activity); Serial.println("  ");
+			//Serial.print("X: "); Serial.print(x); Serial.print("  ");
+			//Serial.print("Y: "); Serial.print(y); Serial.print("  ");
+			//Serial.print("Z: "); Serial.print(z); Serial.println("  ");
+
+			byte interrupts = adxl.getInterruptSource();
+
+			//activity
+			if(adxl.triggered(interrupts, ADXL345_ACTIVITY))
+			{
+				Serial.println("activity");
+				//add code here to do when activity is sensed
+			}
+
+			//inactivity
+			if(adxl.triggered(interrupts, ADXL345_INACTIVITY))
+			{
+				Serial.println("inactivity");
+				//add code here to do when inactivity is sensed
+			}
+
+			sendValues();
+		}
+	}
+}
+
+// Put the Arduino to sleep.
+void sleep()
+{
+	Serial.println("TX Sleepy Time Now");
+	Serial.flush();
+
+	//Shut down the radio
+	driver.sleep();
+
+	// Set sleep to 'full power down'.  Only external interrupts or
+	// the watchdog timer can wake the CPU!
+	set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+
+	// Turn off the ADC while asleep.
+	power_adc_disable();
+
+	// Enable sleep and enter sleep mode.
+	sleep_mode();
+
+	// CPU is now asleep and program execution completely halts!
+	// Once awake, execution will resume at this point.
+
+	// When awake, disable sleep mode and turn on all devices.
+	sleep_disable();
+	power_all_enable();
+
+	//Wake up the radio
+	driver.setModeIdle();
+}
+void sendValues()
+{
+	uint8_t len = sizeof(SensorReadings);
+
+	//Load message into data-array
+	memcpy (buf, &SensorReadings, len);
+
+	if(manager.sendtoWait(buf, len, SERVER_ADDRESS))
+		Serial.println("TX Succesfull 'sendtoWait'");
+	else
+		Serial.println("TX Failed 'sendtoWait'");
+
+	sleep();
+}
+
+void readVoltage()
+{
+	SensorReadings.TX_Voltage = analogRead(BATTERY_PIN);
+}
+
+void handShake()
+{
+	adxl.getInterruptSource();
+
+	readVoltage();
+
+	uint8_t len = sizeof(SensorReadings);
+
+	//Load message into data-array
+	memcpy (buf, &SensorReadings, len);
+
+	while(!RX_responded_OK)
+	{
+		while(!manager.sendtoWait(buf, len, SERVER_ADDRESS))
+		{
+			Serial.println("TX Failed 'sendtoWait'");
+		}
+		Serial.println("TX Succesfull 'sendtoWait'");
+		RX_responded_OK = true;
+	}
+	sleep();
+}
+
+void accSetup()
+{
+	adxl.powerOn();
+	//adxl.set_bw(ADXL345_BW_6);
+	adxl.setLowPower(0);
+	adxl.set_bw(ADXL345_BW_25);
+	//adxl.setActivityAc(0);
+	adxl.setInterruptLevelBit(0);
+
+	//set activity/ inactivity thresholds (0-255)
+	adxl.setActivityThreshold(20); //62.5mg per increment
+	//adxl.setInactivityThreshold(75); //62.5mg per increment
+	//adxl.setTimeInactivity(10); // how many seconds of no activity is inactive?
+
+	//look of activity movement on this axes - 1 == on; 0 == off
+	adxl.setActivityX(1);
+	adxl.setActivityY(1);
+	adxl.setActivityZ(1);
+
+	//look of inactivity movement on this axes - 1 == on; 0 == off
+	//adxl.setInactivityX(1);
+	//adxl.setInactivityY(1);
+	//adxl.setInactivityZ(1);
+
+	adxl.setInterruptMapping(ADXL345_INT_ACTIVITY_BIT, ADXL345_INT1_PIN);
+	//adxl.setInterruptMapping(ADXL345_INT_INACTIVITY_BIT, ADXL345_INT1_PIN);
+
+	adxl.setInterrupt(ADXL345_INT_ACTIVITY_BIT, 1);
+	//adxl.setInterrupt(ADXL345_INT_INACTIVITY_BIT, 0);
+	//adxl.printAllRegister();
 }
 
